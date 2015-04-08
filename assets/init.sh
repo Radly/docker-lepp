9a6887735975f0074902408d133d6fd83fcf0088
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-04-08 09:19:39 +0430

    Start SSH server

--- assets/init.sh
+++ assets/init.sh
@@ -5,7 +5,8 @@ set -e
 case "$1" in
 
 start)
-        /etc/init.d/php5-fpm start
+        /etc/init.d/ssh start
+	/etc/init.d/php5-fpm start
 	/etc/init.d/postgresql start
         nginx -g "daemon off;"
     ;;
