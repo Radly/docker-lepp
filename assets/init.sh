4cc6b3a20bca2352882f0de90b8c092be50dac7c
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-09-17 17:25:05 +0430

    Add djbdns

--- assets/init.sh
+++ assets/init.sh
@@ -1,18 +1,57 @@
 #!/usr/bin/env bash
 set -e
 
-case "$1" in
+RAD_PASSWORD="${RAD_PASSWORD:-radphp}"
+ROOT_PASSWORD="${ROOT_PASSWORD:-rootphp}"
 
-start)
-        service ssh start
-	service php5-fpm start
-	service postgresql start
-        nginx -g "daemon off;"
+appInit () {
+  echo 'Init ...'
+  echo "radphp:$RAD_PASSWORD" | chpasswd
+  echo "root:$ROOT_PASSWORD" | chpasswd
+}
+
+appStart () {
+  appInit
+
+  echo 'Start ...'
+  service ssh start
+  /usr/local/bin/svscanboot
+  service php5-fpm start
+  service postgresql start
+  nginx -g "daemon off;"
+}
+
+appHelp () {
+  echo "Available options:"
+  echo " start          - Starts the lepp server (default)"
+  echo " init           - Initialize the lepp server but don't start it."
+  echo " help           - Displays the help"
+  echo " [command]      - Execute the specified linux command eg. bash."
+}
+
+case "$1" in
+  start)
+    appStart
     ;;
-provision)
-        echo "Hello provisioning"
+  init)
+    appInit
     ;;
-*)
-        exec "$@"
-   ;;
+  help)
+    appHelp
+    ;;
+  *)
+    if [ -x $1 ]; then
+      $1
+    else
+      prog=$(which $1)
+      if [ -n "$prog" ] ; then
+        shift 1
+        ${prog} $@
+      else
+        appHelp
+      fi
+    fi
+    ;;
 esac
+
+exit 0
--- assets/install.sh
+++ assets/install.sh
@@ -1,17 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
-USER=radphp
-GROUP=radphp
-PASS=radphp
+RAD_USER=radphp
+RAD_GROUP=radphp
 
 # Add & config radphp user
-echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-useradd --create-home --user-group -s /usr/bin/zsh ${USER}
-echo "$USER:$PASS"|chpasswd
-sudo -u ${USER} -H sh -c "export SHELL=/usr/bin/zsh; curl -L http://install.ohmyz.sh | bash"
-sudo -u ${USER} -H sh -c "sed -i 's/ZSH_THEME=\".*\"/ZSH_THEME=\"maran\"/g' /home/$USER/.zshrc"
+echo "$RAD_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+useradd --create-home --user-group -s /usr/bin/zsh ${RAD_USER}
+sudo -u ${RAD_USER} -H sh -c "export SHELL=/usr/bin/zsh; curl -L http://install.ohmyz.sh | bash"
+sudo -u ${RAD_USER} -H sh -c "sed -i 's/ZSH_THEME=\".*\"/ZSH_THEME=\"maran\"/g' /home/$RAD_USER/.zshrc"
 
+# Install Djbdns & Daemontools
+mkdir -p /package
+chmod 1755 /package
+cd /package
+curl -SLO http://cr.yp.to/daemontools/daemontools-0.76.tar.gz
+tar -zxvpf daemontools-0.76.tar.gz
+cd admin/daemontools-0.76
+sed -i "s/extern int errno;/#include <errno.h>/g" src/error.h
+package/install
+cd /package
+curl -SLO http://cr.yp.to/djbdns/djbdns-1.05.tar.gz
+tar -zxvpf djbdns-1.05.tar.gz
+cd /package/djbdns-1.05
+sed -i "s/extern int errno;/#include <errno.h>/g" error.h
+make
+make setup check
+useradd --system --no-create-home --shell /bin/false tinydns
+useradd --system --no-create-home --shell /bin/false dnslog
+tinydns-conf tinydns dnslog /etc/tinydns 0.0.0.0
+mkdir /etc/service
+ln -s /etc/tinydns /etc/service
+ln -s /etc/tinydns /service
 
 # Install Adminer
 mkdir -p /srv/tools/adminer
@@ -21,7 +41,6 @@ curl -SLO https://raw.githubusercontent.com/pappu687/adminer-theme/master/admine
 curl -SLO https://raw.githubusercontent.com/pappu687/adminer-theme/master/adminer-bg.png
 mv latest.php index.php
 
-
 # Install phpPgAdmin
 mkdir -p /srv/tools/phppgadmin
 cd /srv/tools/phppgadmin
@@ -29,15 +48,14 @@ git clone git://github.com/phppgadmin/phppgadmin.git .
 mv conf/config.inc.php-dist conf/config.inc.php
 sed -i "s/$conf\['extra_login_security'\] = true/$conf\['extra_login_security'\] = false/g" conf/config.inc.php
 
-
 # PHP Config
 cp /etc/php5/fpm/pool.d/www.conf /etc/php5/fpm/pool.d/radphp.conf
 sed -i "s/\[www\]/\[radphp\]/g" /etc/php5/fpm/pool.d/radphp.conf
-sed -i "s/user = www-data/user = $USER/g" /etc/php5/fpm/pool.d/radphp.conf
-sed -i "s/group = www-data/group = $GROUP/g" /etc/php5/fpm/pool.d/radphp.conf
+sed -i "s/user = www-data/user = $RAD_USER/g" /etc/php5/fpm/pool.d/radphp.conf
+sed -i "s/group = www-data/group = $RAD_GROUP/g" /etc/php5/fpm/pool.d/radphp.conf
 sed -i "s/listen = \/var\/run\/php5-fpm.sock/listen = \/var\/run\/php5-fpm-radphp.sock/g" /etc/php5/fpm/pool.d/radphp.conf
-sed -i "s/listen.owner = www-data/listen.owner = $USER/g" /etc/php5/fpm/pool.d/radphp.conf
-sed -i "s/listen.group = www-data/listen.group = $GROUP/g" /etc/php5/fpm/pool.d/radphp.conf
+sed -i "s/listen.owner = www-data/listen.owner = $RAD_USER/g" /etc/php5/fpm/pool.d/radphp.conf
+sed -i "s/listen.group = www-data/listen.group = $RAD_GROUP/g" /etc/php5/fpm/pool.d/radphp.conf
 sed -i "s/;listen.mode = 0660/listen.mode = 0666/g" /etc/php5/fpm/pool.d/radphp.conf
 sed -i "s/;date.timezone =/date.timezone = Asia\/Tehran/g" /etc/php5/fpm/php.ini
 sed -i "s/;date.timezone =/date.timezone = Asia\/Tehran/g" /etc/php5/cli/php.ini
@@ -48,10 +66,8 @@ sed -i "s/display_errors = Off/display_errors = On/g" /etc/php5/cli/php.ini
 sed -i "s/display_startup_errors = Off/display_startup_errors = On/g" /etc/php5/fpm/php.ini
 sed -i "s/display_startup_errors = Off/display_startup_errors = On/g" /etc/php5/cli/php.ini
 
-
 # Install composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
-
 
 # Config Nginx
 cp /opt/radphp/configs/nginx/default /etc/nginx/sites-available/
